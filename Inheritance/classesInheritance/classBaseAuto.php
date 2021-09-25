a1cf9e2e31117ca3816fd6f1066f74560df4087c
Svyatoslav <asvytoslav@mail.ru>
2021-09-25 23:26:59 +0400

    add hw2

---
+++ Inheritance/classesInheritance/classBaseAuto.php
@@ -0,0 +1,77 @@
+<?php 
+
+class Baseauto
+{
+    protected $name, $color, $type;
+
+    function __construct($name, $color, $type)
+    {
+        $this->setName($name);
+        $this->color = $color;
+        $this->type = $type;
+    }
+
+    
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of color
+     */ 
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * Set the value of color
+     *
+     * @return  self
+     */ 
+    public function setColor($color)
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of type
+     */ 
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set the value of type
+     *
+     * @return  self
+     */ 
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
+
+?>
